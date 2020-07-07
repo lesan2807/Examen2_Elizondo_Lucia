08ff90d39a137097eb48230e30dac06c466dbea2
Lucia Elizondo <lesan2807@gmail.com>
2020-07-07 14:47:58 -0600

    Add model for building pizza

---
+++ Examen2_Elizondo_Lucia/Models/BuildPizzaModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+
+namespace Examen2_Elizondo_Lucia.Models
+{
+    public class BuildPizzaModel
+    {
+
+        string Size { get; set; }
+
+        string Sauce { get; set; }
+
+        string Cheese { get; set; }
+
+        List<string> Ingredients { get; set; }
+    }
+}
