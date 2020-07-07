f2ce7c261e21d2707e94d9752dd699a81dd4bc09
Lucia Elizondo <lesan2807@gmail.com>
2020-07-07 14:29:11 -0600

    Add controller for building a pizza

---
+++ Examen2_Elizondo_Lucia/Controllers/BuildPizzaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Examen2_Elizondo_Lucia.Controllers
+{
+    public class BuildPizzaController : Controller
+    {
+        // GET: BuildPizza
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
